56c4cf5b1af99aa7ef64ac6960d911e7f4189682
Dheeraj Singh <your.email@dheerajsingh2100@gmail.com>
2025-04-24 09:59:27 +0530

    added chat feature

--- src/app.js
+++ src/app.js
@@ -1,45 +1,49 @@
 const express = require("express");
-const connectDb = require("./config/database");
+const connectDB = require("./config/database");
+const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-
-const app = express();
+const http = require("http");
 
 require("dotenv").config();
+
 require("./utils/cronjob");
-
-
 
 app.use(
   cors({
-    origin: "http://localhost:5173",  
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
-
-app.use(cookieParser());
 app.use(express.json());
+app.use(cookieParser());
 
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/requests");
 const userRouter = require("./routes/user");
-const paymentRouter =require("./routes/payment");
+const paymentRouter = require("./routes/payment");
+const initializeSocket = require("./utils/socket");
+const chatRouter = require("./routes/chat");
+const connectDb = require("./config/database");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
-app.use("/",paymentRouter);
+app.use("/", paymentRouter);
+app.use("/", chatRouter);
 
+const server = http.createServer(app);
+initializeSocket(server);
 
-connectDb()
+connectDB()
   .then(() => {
-    console.log("Successfully connected to DB");
-    app.listen(7777, () => {
-      console.log("Server is running on port 7777");
+    console.log("Database connection established...");
+    server.listen(7777, () => {
+      console.log("Server is successfully listening on port 7777...");
     });
   })
   .catch((err) => {
-    console.log("Error in database connection:", err);
+    console.error("Database cannot be connected!!");
   });
